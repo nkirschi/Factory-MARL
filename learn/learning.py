f26b8431a88ebd38780b59d47b0da3e082a17b44
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-05-28 23:43:53 +0200

    Explicitly use subprocess vectorisation

--- learn/learning.py
+++ learn/learning.py
@@ -1,13 +1,15 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from environments import *
 
+if __name__ == "__main__":
+    NUM_ENVS = 8
+    TOTAL_TIMESTEPS = int(1e6)
 
-NUM_ENVS = 8
-TOTAL_TIMESTEPS = int(1e6)
-
-env = make_vec_env(lambda: SingleDeltaEnvWithNormPenalty(render_mode="rgb_array"), n_envs=NUM_ENVS)
-env.reset()
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
-model.save("policies/single_delta_policy_with_norm_penalty")
+    env = make_vec_env(lambda: SingleDeltaEnvWithNormPenalty(render_mode="rgb_array"),
+                       n_envs=NUM_ENVS, vec_env_cls=SubprocVecEnv)
+    env.reset()
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
+    model.save("policies/single_delta_policy_with_norm_penalty")
