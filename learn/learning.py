03219fca92321c54a62c6f3f7ad99c8eaf4d9823
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-05-28 23:10:02 +0200

    Train with norm penalty

--- learn/learning.py
+++ learn/learning.py
@@ -1,10 +1,13 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from environments import TaskEnv, SingleDeltaEnv
+from environments import *
 
-num_envs = 8
-env = make_vec_env(lambda: SingleDeltaEnv(render_mode="rgb_array"), n_envs=num_envs)
+
+NUM_ENVS = 8
+TOTAL_TIMESTEPS = int(1e6)
+
+env = make_vec_env(lambda: SingleDeltaEnvWithNormPenalty(render_mode="rgb_array"), n_envs=NUM_ENVS)
 env.reset()
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=25000, progress_bar=False)
-model.save("single_delta_policy")
+model.learn(total_timesteps=TOTAL_TIMESTEPS, progress_bar=True)
+model.save("policies/single_delta_policy_with_norm_penalty")
