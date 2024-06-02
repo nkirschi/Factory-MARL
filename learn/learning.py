bece73f7ca3ad822cf990f08fcbd87caf485db46
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-06-02 19:22:18 +0200

    Prepare next run

--- learn/learning.py
+++ learn/learning.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     env.reset()
     env = VecVideoRecorder(env,
                            video_folder=f"videos/{run.id}",
-                           record_video_trigger=lambda x: x % 2000 == 0,
-                           video_length=200)
+                           record_video_trigger=lambda x: x % (CONFIG["chkpt_freq"] / CONFIG["num_envs"]) == 0,
+                           video_length=100)
     model = CONFIG["rl_algo"](CONFIG["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
     model.learn(total_timesteps=CONFIG["total_timesteps"],
                 callback=[ProgressBarCallback(), WandbCallback(
