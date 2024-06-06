0ec21cf35a30444c4e0c8878d5c585558df4cefd
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-06-06 17:30:44 +0200

    Prepare next runs

--- learn/learning.py
+++ learn/learning.py
@@ -1,5 +1,6 @@
+from gymnasium.wrappers import NormalizeReward
 from stable_baselines3 import PPO, SAC
-from stable_baselines3.common.callbacks import ProgressBarCallback
+from stable_baselines3.common.callbacks import CheckpointCallback, ProgressBarCallback
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import SubprocVecEnv, VecVideoRecorder
@@ -10,16 +11,27 @@ import os
 import wandb
 
 if __name__ == "__main__":
-    #os.environ["MUJOCO_GL"] = "osmesa"
+    # os.environ["MUJOCO_GL"] = "osmesa"
     CONFIG = {
         "num_envs": 8,
-        "env_class": SingleDeltaEnvWithNormPenaltyFixed,
+        "env_class": SingleDeltaEnv,
         "run_name": "single_delta_policy_with_norm_penalty",
         "rl_algo": PPO,
         "total_timesteps": int(1e6),
         "policy_type": "MlpPolicy",
         "chkpt_freq": 16384,
+        "score_weight": 1,
+        "norm_penalty_weight": 1
     }
+
+
+    def make_env():
+        env = CONFIG["env_class"](CONFIG["score_weight"],
+                                  CONFIG["norm_penalty_weight"],
+                                  render_mode="rgb_array")
+        return Monitor(env)
+
+
     wandb.login(key="f4cdba55e14578117b20251fd078294ca09d974d", verify=True)
     run = wandb.init(project="adlr",
                      name=CONFIG["run_name"],
@@ -27,7 +39,7 @@ if __name__ == "__main__":
                      sync_tensorboard=True,
                      monitor_gym=True,
                      save_code=True)
-    env = make_vec_env(lambda: Monitor(CONFIG["env_class"](render_mode="rgb_array")),
+    env = make_vec_env(make_env,
                        n_envs=CONFIG["num_envs"],
                        vec_env_cls=SubprocVecEnv)
     env.reset()
@@ -37,9 +49,10 @@ if __name__ == "__main__":
     #                        video_length=100)
     model = CONFIG["rl_algo"](CONFIG["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
     model.learn(total_timesteps=CONFIG["total_timesteps"],
-                callback=[ProgressBarCallback(), WandbCallback(
-                    model_save_freq=CONFIG["chkpt_freq"],
-                    model_save_path=f"policies/{run.id}",
-                    verbose=2,
-                )])
+                callback=[  # ProgressBarCallback(),
+                    CheckpointCallback(save_freq=CONFIG["chkpt_freq"],
+                                       save_path=f"policies/{run.id}"),
+                    WandbCallback(model_save_freq=CONFIG["chkpt_freq"],
+                                  model_save_path=f"policies/{run.id}",
+                                  verbose=2)])
     run.finish()
