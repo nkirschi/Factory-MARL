b392a9213eb3a4dbc725bc13da085dc8d1567f6f
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-07-03 13:55:04 +0200

    Update

--- learn/visualisation.py
+++ learn/visualisation.py
@@ -1,23 +1,27 @@
+import itertools
 import time
 from stable_baselines3 import PPO, SAC
 from environments import *
 
-env = SingleDeltaEnv(1, 1, render_mode="human")
+# env = TaskEnv(render_mode="human")
+# env = SingleFullRLEnv(render_mode="human")
+# env = SingleDeltaEnv(1, 1, render_mode="human")
+env = SingleFullRLProgressRewardEnv(gripper_to_closest_cube_reward_factor=0.1,
+                                    closest_cube_to_bucket_reward_factor=0.1,
+                                    base_reward=0,
+                                    small_action_norm_reward_factor=0,
+                                    render_mode="human")
 obs, info = env.reset()
 num_episodes = 10
 
-model = SAC.load("policies/SAC_1_1")
+model = SAC.load("policies/ProgressRewSAC.zip")
 
-frames = []
 for e in range(num_episodes):
     tick = time.time()
-    t = 0
-    while True:
-        t += 1
+    for t in itertools.count():
         action, _states = model.predict(obs)
         obs, reward, terminate, truncate, info = env.step(action)
-        print("action:", action)
-        print("reward:", reward)
+        print(f"reward: {reward:.4f}")
         if terminate:
             print(
                 f"Episode {e}, Score: {info['scores']}, FPS: {t / (time.time() - tick):.2f}"
