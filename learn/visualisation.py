8878a926c21b39a8140d4568ed0065031358f386
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-07-21 20:19:22 +0200

    Add summary metric logging

--- learn/visualisation.py
+++ learn/visualisation.py
@@ -1,23 +1,27 @@
 import environments
 import itertools
+import numpy
 import shutil
 import stable_baselines3
 import time
+import tqdm
 import wandb
 
 # BEGIN configurable part #
 
-RUN_ID = None #"vwq53k5z"
+RUN_ID = None  # "y6lp1j7k"
 CHECKPOINT = 10 / 10  # available: 1 to 10 out of 10
 NUM_EPISODES = 10
+RENDER_MODE = "human"  # either "human" or "rgb_array"
 RESOLUTION = 1024
+VERBOSE = True
 
 # END configurable part #
 
 
 if __name__ == "__main__":
     if RUN_ID is None:
-        env = environments.TaskEnv(render_mode="human", width=RESOLUTION, height=RESOLUTION, num_arms=4)
+        env = environments.TaskEnv(render_mode=RENDER_MODE, width=RESOLUTION, height=RESOLUTION, num_arms=4)
     else:
         wandb.login(key="f4cdba55e14578117b20251fd078294ca09d974d", verify=True)
         api = wandb.Api()
@@ -26,7 +30,8 @@ if __name__ == "__main__":
         model_file = run.file(f"checkpoints/rl_model_{chkpt_timestep}_steps.zip").download("/tmp", replace=True)
         shutil.copy(model_file.name, f"policies/{RUN_ID}.zip")
 
-        run.config["env_kwargs"]["render_mode"] = "human"
+
+        run.config["env_kwargs"]["render_mode"] = RENDER_MODE
         run.config["env_kwargs"]["width"] = RESOLUTION
         run.config["env_kwargs"]["height"] = RESOLUTION
 
@@ -34,8 +39,10 @@ if __name__ == "__main__":
         model = getattr(stable_baselines3, run.config["rl_algo"]).load(f"policies/{RUN_ID}.zip")
 
     obs, info = env.reset()
+    ep_lens = []
+    ep_scores = []
 
-    for e in range(NUM_EPISODES):
+    for e in range(NUM_EPISODES) if VERBOSE else tqdm.tqdm(range(NUM_EPISODES)):
         tick = time.time()
         for t in itertools.count():
             if RUN_ID is None:
@@ -43,14 +50,19 @@ if __name__ == "__main__":
             else:
                 action, _states = model.predict(obs)
             obs, reward, terminate, truncate, info = env.step(action)
-            print(f"IK states: {tuple(env.ik_policies[i].state.name for i in range(env.num_arms))}")
-            print(f"action: {action}")
-            print(f"reward: {reward:.4f}")
+            if VERBOSE:
+                print(f"IK states: {tuple(env.ik_policies[i].state.name for i in range(env.num_arms))}")
+                print(f"action: {action}")
+                print(f"reward: {reward:.4f}")
             if terminate:
-                print(
-                    f"Episode {e}, Score: {info['scores']}, FPS: {t / (time.time() - tick):.2f}"
-                )
+                if VERBOSE:
+                    print(f"Episode {e}, Score: {info['scores']}, FPS: {t / (time.time() - tick):.2f}")
+                ep_scores.append(info['scores'])
+                ep_lens.append(t)
                 env.reset()
                 break
+
+    print("avg episode scores:", numpy.array(ep_scores).mean(axis=0))
+    print("avg episode lengths", numpy.array(ep_lens).mean())
 
     env.close()
